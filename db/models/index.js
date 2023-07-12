5e574b9852c67119bd62aeb500018f53e202f8fb
yuzchen7 <yuzchen7@gmail.com>
2023-07-12 11:38:15 -0400

    feat : set-up the relationship between flight and user

--- db/models/index.js
+++ db/models/index.js
@@ -1,5 +1,18 @@
 const User = require("./user");
+const Flights = require("./flights");
+
+// relationship between User and Flights
+// M:N relationship
+User.belongsToMany(Flights, {
+  through : 'user_flight',
+  foreignKey : 'user_id'
+});
+
+Flights.belongsToMany(User, {
+  through : 'user_flight',
+  foreignKey : 'flight_number'
+});
 
 module.exports = {
-  User
+  User, Flights
 };
