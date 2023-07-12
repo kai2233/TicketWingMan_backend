70bc5932c291c86ff9354545e46f298d4a31d487
9Mohamedismail <87681402+9Mohamedismail@users.noreply.github.com>
2023-07-12 12:03:28 -0400

    API Complete: GET, DELETE, POST, PUT functions created

---
+++ api/user.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require("../db/models");
+
+// Handles GET of all users: localhost:8080/api/user/
+router.get("/", async (req, res, next) => {
+  try {
+    const allUsers = await User.findAll({ attributes: ["id", "email"] });
+
+    allUsers
+      ? res.status(200).json(allUsers)
+      : res.status(400).send("User List Not Found");
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Handles DELETE of a specific user using their email: localhost:8080/api/user/email-goes-here
+router.delete("/:email", async (req, res, next) => {
+  try {
+    const userEmail = req.params.email;
+    const foundUser = await User.findAll({ where: { email: userEmail } });
+
+    if (!foundUser) {
+      res.status(404).send("User not found");
+    } else {
+      await User.destroy({ where: { email: userEmail } });
+      res.status(200).send("User deleted successfully");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Handles GET of a specific user using their email: localhost:8080/api/user/email-goes-here
+router.get("/:email", async (req, res, next) => {
+  try {
+    const userEmail = req.params.email;
+    console.log(userEmail);
+    const foundUser = await User.findAll({ where: { email: userEmail } }); //fetching a User student from database
+    foundUser
+      ? res.status(200).json(foundUser)
+      : res.status(404).send("User Not Found");
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Handles POST of a new user: localhost:8080/api/user/
+router.post("/", async (req, res, next) => {
+  try {
+    const results = await User.create(req.body);
+    if (!results) {
+      res.status(400).send("Failed to create User");
+    } else {
+      res.status(200).send("User added successfully");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:email", async (req, res, next) => {
+  try {
+    const userEmail = req.params.email;
+    const foundUser = await User.findAll({ where: { email: userEmail } });
+
+    if (!foundUser) {
+      res.status(400).send("Failed to update User");
+    } else {
+      await User.update(req.body, { where: { email: userEmail } });
+      res.status(200).send("User updated successfully");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const express = require('express');
+const express = require("express");
 const db = require("./db");
 const app = express();
 const port = 8080;
-const cors = require('cors');
-app.use(cors());
-app.use(express.json());
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-    res.send('Hello! This is ticketWingMan backend')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Mount on API
+app.use("/api", require("./api"));
+
+app.get("/", (req, res) => {
+  res.send("Hello! This is ticketWingMan backend");
 });
 
-const runServer = () => {
-    app.listen(port, () => {
-        console.log(`server is running on port 8080`)
-    })
-};
+// Syncing DB Function
+const syncDB = () => db.sync({ force: true });
 
-const syncDB = () => db.sync({force: true});
+// Run Server Function
+const runServer = () => {
+  app.listen(port, () => {
+    console.log(`server is running on port 8080`);
+  });
+};
 
 syncDB();
 runServer();
-module.exports = app;;
+module.exports = app;
