fc2bc765a99f15296f77789914495098febae6f0
Kaifeng <1536771545kai@gmail.com>
2023-07-12 15:51:12 -0400

    add singup feature

--- auth/index.js
+++ auth/index.js
@@ -1,16 +1,33 @@
 const router = require("express").Router();
-const {User} = require("../db/models");
+const { User } = require("../db/models");
 
-router.post('/login',async (req, res,next) => {
-    try{
-        const user = await User.findAll({where:{email:req.body.email}});
-        if (user||user.validatePassword(req.body.password)){
-            res.login(user,err => (err?next(err):res.status(200).json(user)));
-        }else{
+router.post('/login', async (req, res, next) => {
+    try {
+        const user = await User.findAll({ where: { email: req.body.email } });
+        if (user || user.validatePassword(req.body.password)) {
+            res.login(user, err => (err ? next(err) : res.status(200).json(user)));
+        } else {
             res.status(401).send("Invalid login. Please try again")
         }
-    }catch(err){
+    } catch (err) {
         next(err);
+    }
+});
+
+router.post('/signup', async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).send("required fields are mssing");
+        }
+        const user = await User.create(req.body);
+        req.login(user, (err) => (err ? next(err) : res.status(200).json(user)));
+    } catch (error) {
+        if (error.name === "SequelizeUniqueConstraintError") {
+            res.status(409).send("User already exists");
+        } else {
+            next(error);
+        }
     }
 });
 
