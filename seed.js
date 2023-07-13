463c715f1d89f310a60d57581fab35b930a98a53
yuzchen7 <yuzchen7@gmail.com>
2023-07-13 15:23:57 -0400

    test : add flight test data

--- seed.js
+++ seed.js
@@ -34,8 +34,31 @@ const UserSeed = [
     },
 ];
 
+const FlightsSeed = [
+    {
+        flight_number : 'TP487',
+        departure_date : '2023-08-01 12:10:00-04',
+        departure_location : 'NCE', 
+        arrival_date : '2023-08-01 14:45:00',
+        arrival_location : 'LIS',
+        emissions : 0,
+        userId : 1 
+    },
+
+    {
+        flight_number : 'CX840',
+        departure_date : '2023-08-01 04:15:00-0',
+        departure_location : 'HKG', 
+        arrival_date : '2023-08-01 20:25:00',
+        arrival_location : 'JFK',
+        emissions : 0,
+        userId : 1 
+    },
+];
+
 const seed = async () => {
     await User.bulkCreate(UserSeed);
+    await Flights.bulkCreate(FlightsSeed);
 };
 
 seed().then(() => process.exit());
