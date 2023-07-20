a95cb54c656687770e220f020e1ae4e34177a04d
Kaifeng <1536771545kai@gmail.com>
2023-07-19 20:15:35 -0400

    add rain sum and snowfall sum to api request

--- api/weather.js
+++ api/weather.js
@@ -19,7 +19,7 @@ router.post("/displayInFahrenheit", async (req, res, next) => {
         const latitude = location.data.results[0].latitude;
         const longitude = location.data.results[0].longitude;
         const result = await axios.get
-            (`https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${date1}&end_date=${date2}&daily=temperature_2m_max&temperature_unit=fahrenheit&timezone=America%2FNew_York`);
+            (`https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${date1}&end_date=${date2}&daily=temperature_2m_max,rain_sum,snowfall_sum&temperature_unit=fahrenheit&timezone=America%2FNew_York`);
         result
             ? res.status(200).json(result.data)
             : res.status(400).send("result not found");
@@ -38,7 +38,7 @@ router.post("/displayInCelsius", async (req, res, next) => {
         const latitude = location.data.results[0].latitude;
         const longitude = location.data.results[0].longitude;
         const result = await axios.get
-            (`https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${date1}&end_date=${date2}&daily=temperature_2m_max&timezone=America%2FNew_York`);
+            (`https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${date1}&end_date=${date2}&daily=temperature_2m_max,rain_sum,snowfall_sum&timezone=America%2FNew_York`);
         result
             ? res.status(200).json(result.data)
             : res.status(400).send("result not found");
