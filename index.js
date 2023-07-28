bea86888cd54bfa1a5c089af106b055e81a0aafa
9Mohamedismail <9Mohamedismail@gmail.com>
2023-07-28 16:36:22 -0400

    Changed Cors origin

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://ticket-wingman.vercel.app",
     methods: "GET,PUT,PATCH,HEAD,POST,DELETE",
     credentials: true,
   })
