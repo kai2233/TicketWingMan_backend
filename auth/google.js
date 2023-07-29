6fcdc4e0d355e10e1e661127522e71878dc138e2
Kaifeng <1536771545kai@gmail.com>
2023-07-29 17:29:36 -0400

    modify github & google redirect url

--- auth/google.js
+++ auth/google.js
@@ -39,11 +39,11 @@ router.get(
   "/callback",
   passport.authenticate("google", {
     // failureRedirect: "http://localhost:3000/login",
-    failureRedirect: "https://64c55a9b23a9c3138c205eea--ubiquitous-meerkat-f62c9c.netlify.app/login",
+    failureRedirect: "https://ticket-wingman.netlify.app/login",
   }),
   (req, res) => {
     // res.redirect("http://localhost:3000");
-    res.redirect("https://64c55a9b23a9c3138c205eea--ubiquitous-meerkat-f62c9c.netlify.app");
+    res.redirect("https://ticket-wingman.netlify.app");
   }
 );
 
