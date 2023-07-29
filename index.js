d26f4b5570753f0e10cf4ee4903881930bbeb598
Kaifeng <1536771545kai@gmail.com>
2023-07-28 22:36:06 -0400

    add app.use res.setHeader

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const store = new sequelizeStore({ db });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://ticket-wingman.vercel.app/');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 app.use(
   cors({
     // origin: "https://ticket-wingman.vercel.app",
