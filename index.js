6c6dce0c5c4176a1e0c55dba1f370129d86bd31d
Kaifeng <1536771545kai@gmail.com>
2023-07-29 14:31:29 -0400

    change cors origin to netlify deployed frontend

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://64c55a9b23a9c3138c205eea--ubiquitous-meerkat-f62c9c.netlify.app",
     methods: "GET,PUT,PATCH,HEAD,POST,DELETE",
     credentials: true,
   })
